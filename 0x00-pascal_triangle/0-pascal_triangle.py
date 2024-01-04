d8ace85aba23505b3787809fd232cd20b91a7312
TobiLight <jgetitdonefast@gmail.com>
2024-01-04 18:59:29 +0100

    ✔ Returns a list of lists of integers representing the Pascal’s triangle of n

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# File: 0-pascal_triangle.py
-# Author: Oluwatobiloba Light
-# Returns a list of lists of integers representing the Pascal’s triangle of n
 
 
 def pascal_triangle(n):
