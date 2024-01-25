5d321def7288639201f13d1625f13fd14acd8ab3
TobiLight <jgetitdonefast@gmail.com>
2024-01-25 23:31:57 +0100

    ✔ Script that reads stdin line by line and computes metrics

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -35,10 +35,10 @@ def get_line_details(line: str):
     Returns ip, code and file size from line
     """
     ip = re.match(r'(\d+\.\d+\.\d+\.\d+)', line).group()
-    code, file_size = re.match(
+    code, file_size = re.search(
         r' - \[.*\] "GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+)', line)\
         .groups()
-    return (ip, code, file_size)
+    return (ip, str(code), str(file_size))
 
 
 try:
