023d6c2fe2960b44fc0bada03a3f14a56d335170
TobiLight <jgetitdonefast@gmail.com>
2024-02-29 18:20:04 +0100

    ✔ Finds the fewest number of coins needed to meet a given total amount.

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -45,7 +45,7 @@ def makeChange(coins, total):
 
     for amount in range(1, total + 1):
         for coin in coins:
-            if coin <= amount:
+            if amount - coin >= 0:
                 answers[amount] = min(
                     answers[amount], answers[amount - coin] + 1)
 
