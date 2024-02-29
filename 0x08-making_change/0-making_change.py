d2e256e41adda526633db2e3a0ff1df7eec102b7
TobiLight <jgetitdonefast@gmail.com>
2024-02-29 17:13:19 +0100

    ✔ Finds the fewest number of coins needed to meet a given total amount.

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -19,18 +19,37 @@ def makeChange(coins, total):
         The fewest number of coins needed to meet the total,
         or -1 if it cannot be met.
     """
+    # Greedy approach
+    # if total <= 0:
+    #     return 0
+
+    # sorted_coins = sorted(coins, reverse=True)
+    # counter = 0
+
+    # for coin in sorted_coins:
+    #     while total >= coin:
+    #         counter += 1
+    #         total -= coin
+
+    # if total == 0:
+    #     return counter
+
+    # return -1
+
+    # Dynamic programming
     if total <= 0:
         return 0
 
-    sorted_coins = sorted(coins, reverse=True)
-    counter = 0
+    answers = [float('inf')] * (total + 1)
+    answers[0] = 0
 
-    for coin in sorted_coins:
-        while total >= coin:
-            counter += 1
-            total -= coin
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if coin <= amount:
+                answers[amount] = min(
+                    answers[amount], answers[amount - coin] + 1)
 
-    if total == 0:
-        return counter
+    if answers[total] != float('inf'):
+        return answers[total]
 
     return -1
