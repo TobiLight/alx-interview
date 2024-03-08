24d35fa571ccca661436f3221a9644cb4877ec93
TobiLight <jgetitdonefast@gmail.com>
2024-03-08 03:43:58 +0100

    ✔ Functiom that returns the perimeter of the island as an integer.

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -24,9 +24,10 @@ def island_perimeter(grid):
             if grid[i][j] == 1:
                 perimeter += 4
 
-                # Check neighboring cells
                 if i > 0 and grid[i - 1][j] == 1:
                     perimeter -= 2
+
                 if j > 0 and grid[i][j - 1] == 1:
                     perimeter -= 2
+
     return perimeter
