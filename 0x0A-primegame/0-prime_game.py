145fca575177b1af92b638502693fcbd0f73ac39
TobiLight <jgetitdonefast@gmail.com>
2024-03-15 00:51:57 +0000

    ✔ Determines the winner of the game based on the number of rounds

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -35,17 +35,23 @@ def isWinner(x, nums):
     #                 ben_wins += 1
 
     n = max(nums)
+
     primes = [True for _ in range(1, n + 1, 1)]
+
     primes[0] = False
+
     for i, is_prime in enumerate(primes, 1):
         if i == 1 or not is_prime:
             continue
+
         for j in range(i + i, n + 1, i):
             primes[j - 1] = False
-    # filter the number of primes less than n in nums for each round
+
     for _, n in zip(range(x), nums):
         primes_count = len(list(filter(lambda x: x, primes[0: n])))
+
         ben_wins += primes_count % 2 == 0
+
         maria_wins += primes_count % 2 == 1
 
     if maria_wins > ben_wins:
