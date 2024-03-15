fb85d894972aafd99acf3e4b6456618c45268fa9
TobiLight <jgetitdonefast@gmail.com>
2024-03-15 00:36:05 +0000

    ✔ Determines the winner of the game based on the number of rounds

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,6 +4,51 @@
 """Prime Game"""
 
 
+def is_prime(num):
+    """
+    Check if a number is prime.
+
+    Args:
+    num (int): The number to check.
+
+    Returns:
+    bool: True if the number is prime, False otherwise.
+    """
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def prime_game_winner(n):
+    """
+    Determine the winner of the prime game for a given n.
+
+    Args:
+    n (int): The upper limit of the consecutive integers set.
+
+    Returns:
+    str: The name of the player who wins the game ("Maria" or "Ben").
+    """
+    if n <= 1:
+        return "Ben"
+
+    primes = [i for i in range(2, n + 1) if is_prime(i)]
+    dp = [False] * (n + 1)
+    dp[0] = False
+    dp[1] = True
+
+    for i in range(2, n + 1):
+        for prime in primes:
+            if i - prime >= 0 and not dp[i - prime]:
+                dp[i] = True
+                break
+
+    return "Maria" if dp[n] else "Ben"
+
+
 def isWinner(x, nums):
     """
     This function determines the winner of the game based on the number of
@@ -19,9 +64,12 @@ def isWinner(x, nums):
     """
     if x < 1 or not nums:
         return None
+
     maria_wins = ben_wins = 0
+
     for _ in range(x):
         num = nums.pop(0)
+
         if num % 2 == 0:
             ben_wins += 1
         else:
